4daabc5469c07e4913f376ab12afd601d42e007a
VictorNVK <140235875+VictorNVK@users.noreply.github.com>
2023-10-14 00:32:21 +0500

    Tutor

---
+++ Tutorial.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+@onready var dialoge_box1 = $DialogueBox
+@onready var buttonForMove = $"ButtonForMove/A&D"
+@onready var jump = $ButtonForMove/W
+@onready var dash = $ButtonForMove/Dash
+
+func _ready():
+	buttonForMove.visible = false
+	jump.visible = false
+	dash.visible = false
+	dialoge_box1.start("TUTOR")
+
+func _on_dialogue_box_dialogue_signal(value):
+		if value == "buttons":
+			print("detected!")
+			buttonForMove.visible = true
+		elif value == "disableButtons":
+			buttonForMove.visible = false
+		elif value == "showW":
+			jump.visible = true
+		elif value == "disableW":
+			jump.visible = false
+		elif value == "showDash":
+			dash.visible = true
+		elif value == "end":
+			dash.visible = false
