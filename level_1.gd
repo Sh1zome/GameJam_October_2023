59ba9cc5aafeaaa1ae1f20c9e2c60fa619e844e8
Sh1zome <shizomech@gmail.com>
2023-10-15 16:48:52 +0500

    level_1 dark perehod

--- level_1.gd
+++ level_1.gd
@@ -1,10 +1,28 @@
 extends Node2D
 @onready var dialog = $PlayerAndUI/CanvasLayer/DashColdown/Tutorial/DialogueBox
+@onready var dark = $dark
+
+var timer = 0
+var exit_timer = 0
+var entered = false
+var exited = false
+
 func _ready():
+	entered = true
 	dialog.start("TUTOR")
 
-func _on_perehod_body_entered(body):
-		if body.name == "PlayerHuman":
-			print("Чё?")
+func _process(delta):
+	timer = timer + (delta * 1)
+	if entered && timer >= 0.5:
+		dark.self_modulate = dark.self_modulate.lerp(Color(1,1,1,0), delta * 2)
+	elif exited:
+		exit_timer = exit_timer + (delta * 1)
+		dark.self_modulate = dark.self_modulate.lerp(Color(1,1,1,1), delta * 8)
+		if exit_timer >= 0.6:
 			get_tree().change_scene_to_file("res://lev_2.tscn")
+
+func _on_perehod_body_entered(body):
+	if body.name == "PlayerHuman":
+		entered = false
+		exited = true
 		
