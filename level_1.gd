f00e64f479a3aa29cdd5d450924e46409e310eb9
VictorNVK <140235875+VictorNVK@users.noreply.github.com>
2023-10-15 13:46:26 +0500

    push

--- level_1.gd
+++ level_1.gd
@@ -1,6 +1,10 @@
 extends Node2D
+@onready var dialog = $PlayerAndUI/CanvasLayer/DashColdown/Tutorial/DialogueBox
+func _ready():
+	dialog.start("TUTOR")
 
 func _on_perehod_body_entered(body):
 		if body.name == "PlayerHuman":
+			print("Чё?")
 			get_tree().change_scene_to_file("res://Виталя/level_2.tscn")
 		
