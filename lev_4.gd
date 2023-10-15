d16f4a693328a74c010dfa0dd02edc035701cc41
VictorNVK <victorad498@gmail.com>
2023-10-15 23:52:33 +0500

    SettingDialoge

---
+++ lev_4.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var dialog = $PlayerAndUI/CanvasLayer/DashColdown/Comp
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#Коробка 1
+func _on_box_1_body_entered(body):
+	if body.name == "PlayerHuman":
+		dialog.start("Box1")
+
+#Серверная
+func _on_box_2_body_entered(body):
+	pass # Replace with function body.
+
+#Коробка 3
+func _on_box_3_body_entered(body):
+	pass # Replace with function body.
+
+#Возвращение
+func _on_backto_3_body_entered(body):
+	if body.name == "PlayerHuman":
+		GloabalTreker.tp = true;
+		get_tree().change_scene_to_file("res://lev_3.tscn")
