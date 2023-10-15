1f25f9715d3ebe7d3864f4c3ef74038512313443
VictorNVK <140235875+VictorNVK@users.noreply.github.com>
2023-10-15 16:10:40 +0500

    2 to 1 and hp_bar

--- level_1.gd
+++ level_1.gd
@@ -2,7 +2,10 @@ extends Node2D
 @onready var dialog = $PlayerAndUI/CanvasLayer/DashColdown/Tutorial/DialogueBox
 func _ready():
 	dialog.start("TUTOR")
-
+func _process(delta):
+	if GloabalTreker.tp == true:
+		$PlayerAndUI.rect_posiont.x = 300
+		GloabalTreker.tp = false
 func _on_perehod_body_entered(body):
 		if body.name == "PlayerHuman":
 			print("Чё?")
